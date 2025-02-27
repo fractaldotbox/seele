1ea680f7c13349e8046687f1a3b46c0c774ccd54
debuggingfuture (Vincent LCY) <lauchunyin@debuggingfuture.com>
2025-02-27 10:30:41 -0700

    Df/ethstorage (#10)

---
+++ packages/agents/src/agents/manager.test.ts
@@ -0,0 +1,25 @@
+import { createAgent } from "@statelyai/agent";
+import type { Storage } from "unstorage";
+import { beforeAll, describe, expect, it } from "vitest";
+import { waitFor } from "xstate";
+import { TOPICS_ETH } from "../fixture";
+import { newsAgencyMachine } from "../news-state";
+import { initStorage } from "../storage";
+import { createEmbeddings, queryWithEmbeddings } from "../utils";
+import { crawlNewsWithTopic } from "./curator";
+import { deployArticles } from "./manager";
+
+describe(
+    "ManagerAgent",
+    () => {
+        it('deploy', async () => {
+
+            const articles = [
+                'article content 1',
+                'article content 2',
+            ]
+            await deployArticles(articles);
+
+        })
+
+    }, 60 * 1000);
--- packages/data-fetch/vitest.config.ts
+++ packages/data-fetch/vitest.config.ts
@@ -1,17 +1,7 @@
 import { resolve } from "node:path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		environment: "jsdom",
-		globals: true,
-		coverage: {
-			reporter: ["text", "json", "html"],
-			exclude: ["node_modules/**", "dist/**"],
-		},
-	},
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, "./src"),

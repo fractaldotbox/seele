72bc3dee5085003cd8479bb395479662b922cdf1
metalboyrick <rsulisthio@gmail.com>
2025-02-28 16:20:28 -0700

    fix: use local storage

--- apps/cms/src/lib/repository/token-gate.repository.ts
+++ apps/cms/src/lib/repository/token-gate.repository.ts
@@ -1,115 +1,52 @@
 import type { TokenGateCriteria } from "@seele/access-gate/lib/token-gate/types";
-import {
-	FileType,
-	createDirectory,
-	downloadFileFromDirectoryAsync,
-	uploadFileToDirectory,
-} from "@seele/data-fetch/ethstorage";
-import type { FlatDirectory } from "ethstorage-sdk";
+
+let criteria: TokenGateCriteria[] = [
+  // {
+  //   contractAddress: "0xd369B2b99CC98FC25aF686e132fB10dE5C7349a6",
+  //   tokenType: "ERC20",
+  //   chainId: 84532,
+  //   minBalance: "1",
+  // },
+];
 
 export class TokenGateRepository {
-	private directory: {
-		flatDirectory: FlatDirectory;
-		directoryAddress: string;
-	};
+  static async create(): Promise<TokenGateRepository> {
+    return new TokenGateRepository();
+  }
 
-	constructor(flatDirectory: FlatDirectory, directoryAddress: string) {
-		this.directory = {
-			flatDirectory: flatDirectory,
-			directoryAddress: directoryAddress,
-		};
-	}
+  /**
+   * Adds a new token gate to the repository
+   * @param tokenGateData - The token gate data to be added
+   * @returns The created token gate entity
+   */
+  async add(tokenGateCriteria: TokenGateCriteria[]): Promise<boolean> {
+    try {
+      criteria.push(...tokenGateCriteria);
+      return true;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      throw new Error(`[add] Failed to add token gate: ${err.message}`);
+    }
+  }
 
-	static async create() {
-		console.log(
-			"process.env.PRIVATE_KEY_STORAGE",
-			process.env.PRIVATE_KEY_STORAGE,
-		);
-		console.log(
-			"process.env.DIRECTORY_ADDRESS_MANAGER",
-			process.env.DIRECTORY_ADDRESS_MANAGER,
-		);
+  /**
+   * Deletes a token gate by its ID
+   * @param id - The ID of the token gate to delete
+   * @returns boolean indicating if the deletion was successful
+   */
+  async delete(contractAddress: string): Promise<boolean> {
+    try {
+      criteria = criteria.filter(
+        (token) => token.contractAddress !== contractAddress,
+      );
+      return true;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      throw new Error(`[delete] Failed to delete token gate: ${err.message}`);
+    }
+  }
 
-		const _directory = await createDirectory(
-			process.env.PRIVATE_KEY_STORAGE!,
-			process.env.DIRECTORY_ADDRESS_MANAGER!,
-		);
-
-		return new TokenGateRepository(
-			_directory.flatDirectory,
-			_directory.directoryAddress,
-		);
-	}
-
-	/**
-	 * Adds a new token gate to the repository
-	 * @param tokenGateData - The token gate data to be added
-	 * @returns The created token gate entity
-	 */
-	async add(tokenGateCriteria: TokenGateCriteria[]): Promise<boolean> {
-		try {
-			const existingCriteria = (await this.getAll()) || [];
-
-			const request = {
-				key: "abc.json",
-				content: Buffer.from(
-					JSON.stringify([...existingCriteria, ...tokenGateCriteria]),
-				),
-				type: FileType.Blob,
-			};
-
-			await uploadFileToDirectory(
-				process.env.PRIVATE_KEY_STORAGE!,
-				this.directory.directoryAddress,
-				request,
-			);
-
-			return true;
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		} catch (err: any) {
-			throw new Error(`[add] Failed to add token gate: ${err.message}`);
-		}
-	}
-
-	/**
-	 * Deletes a token gate by its ID
-	 * @param id - The ID of the token gate to delete
-	 * @returns boolean indicating if the deletion was successful
-	 */
-	async delete(contractAddress: string): Promise<boolean> {
-		try {
-			const existingCriteria = (await this.getAll()) || [];
-			const updatedCriteria = existingCriteria.filter(
-				(token) => token.contractAddress !== contractAddress,
-			);
-
-			const request = {
-				key: "abc.json",
-				content: Buffer.from(JSON.stringify(updatedCriteria)),
-				type: FileType.Blob,
-			};
-
-			await uploadFileToDirectory(
-				process.env.PRIVATE_KEY_STORAGE!,
-				this.directory.directoryAddress,
-				request,
-			);
-
-			return true;
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		} catch (err: any) {
-			throw new Error(`[delete] Failed to delete token gate: ${err.message}`);
-		}
-	}
-
-	async getAll(): Promise<TokenGateCriteria[]> {
-		const results = await downloadFileFromDirectoryAsync(
-			this.directory.flatDirectory,
-			"abc.json",
-		);
-
-		const resultsInString = JSON.parse(results.toString("utf-8"));
-
-		return resultsInString;
-	}
+  async getAll(): Promise<TokenGateCriteria[]> {
+    return criteria;
+  }
 }

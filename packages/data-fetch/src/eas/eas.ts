af56488376826d0ed9f68249cc138f6fe6098493
metalboyrick <rsulisthio@gmail.com>
2025-03-01 06:41:10 -0700

    fix: access control fix

--- packages/data-fetch/src/eas/eas.ts
+++ packages/data-fetch/src/eas/eas.ts
@@ -5,56 +5,118 @@ import type { Attestation, EASAttestedByMemberVerifierMetadata } from "./types";
 import { getChainNameFromId, getEasDataByChain } from "./utils";
 
 async function _searchAttestationsByParams(
-	chainId: number,
+  chainId: number,
 ): Promise<Attestation[]> {
-	const chainName = getChainNameFromId(chainId);
-	const { easAttestedByMemberSchema, gqlBaseUrl } =
-		getEasDataByChain(chainName);
+  const chainName = getChainNameFromId(chainId);
+  const { easAttestedByMemberSchema, gqlBaseUrl } =
+    getEasDataByChain(chainName);
 
-	const variables = {
-		where: {
-			revoked: {
-				equals: false,
-			},
-			decodedDataJson: {
-				equals: JSON.stringify([
-					{
-						name: "scope",
-						type: "string[]",
-						signature: "string[] scope",
-						value: {
-							name: "scope",
-							type: "string[]",
-							value: [Permissions.ProvideInfo],
-						},
-					},
-				]),
-			},
-			schemaId: {
-				equals: easAttestedByMemberSchema,
-			},
-		},
-	};
+  const variables1 = {
+    where: {
+      revoked: {
+        equals: false,
+      },
+      decodedDataJson: {
+        equals: JSON.stringify([
+          {
+            name: "scope",
+            type: "string[]",
+            signature: "string[] scope",
+            value: {
+              name: "scope",
+              type: "string[]",
+              value: [Permissions.ProvideInfo],
+            },
+          },
+        ]),
+      },
+      schemaId: {
+        equals: easAttestedByMemberSchema,
+      },
+    },
+  };
 
-	try {
-		const { data } = await rawRequest<{
-			attestations: Attestation[];
-		}>(gqlBaseUrl, GetAttestationByParams, variables);
+  const variables2 = {
+    where: {
+      revoked: {
+        equals: false,
+      },
+      decodedDataJson: {
+        equals: JSON.stringify([
+          {
+            name: "scope",
+            type: "string[]",
+            signature: "string[] scope",
+            value: {
+              name: "scope",
+              type: "string[]",
+              value: [Permissions.ProvideInfo, Permissions.Vote],
+            },
+          },
+        ]),
+      },
+      schemaId: {
+        equals: easAttestedByMemberSchema,
+      },
+    },
+  };
 
-		return data.attestations;
-	} catch (error) {
-		console.error("GraphQL query error:", error);
-		return [];
-	}
+  const variables3 = {
+    where: {
+      revoked: {
+        equals: false,
+      },
+      decodedDataJson: {
+        equals: JSON.stringify([
+          {
+            name: "scope",
+            type: "string[]",
+            signature: "string[] scope",
+            value: {
+              name: "scope",
+              type: "string[]",
+              value: [Permissions.Vote, Permissions.ProvideInfo],
+            },
+          },
+        ]),
+      },
+      schemaId: {
+        equals: easAttestedByMemberSchema,
+      },
+    },
+  };
+
+  try {
+    const { data: data1 } = await rawRequest<{
+      attestations: Attestation[];
+    }>(gqlBaseUrl, GetAttestationByParams, variables1);
+
+    const { data: data2 } = await rawRequest<{
+      attestations: Attestation[];
+    }>(gqlBaseUrl, GetAttestationByParams, variables2);
+
+    const { data: data3 } = await rawRequest<{
+      attestations: Attestation[];
+    }>(gqlBaseUrl, GetAttestationByParams, variables3);
+
+    return [
+      ...data1.attestations,
+      ...data2.attestations,
+      ...data3.attestations,
+    ];
+  } catch (error) {
+    console.error("GraphQL query error:", error);
+    return [];
+  }
 }
 
 export async function getCMSWhitelistedAddresses(chainId: number) {
-	const attestations = await _searchAttestationsByParams(chainId);
+  const attestations = await _searchAttestationsByParams(chainId);
 
-	const whitelistedAddresses = attestations.map((attestation) => {
-		return attestation.recipient;
-	});
+  const whitelistedAddresses = attestations.map((attestation) => {
+    return attestation.recipient;
+  });
 
-	console.log("whitelistedAddresses", whitelistedAddresses);
-	return whitelistedAddresses;
+  console.log("whitelistedAddresses", whitelistedAddresses);
+  return whitelistedAddresses;
 }

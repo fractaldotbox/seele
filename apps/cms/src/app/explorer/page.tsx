14a64d8712fbe5625d9bf8cb77529861327b8807
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-02-28 21:02:08 -0700

    add: article list

--- apps/cms/src/app/explorer/page.tsx
+++ apps/cms/src/app/explorer/page.tsx
@@ -28,21 +28,16 @@ const ExplorerPage = () => {
 
     if (!mounted) return null;
 
-    if (!address) {
-        return (
-            <div className="flex items-center justify-center min-h-screen">
-                <p className="text-xl">Please connect your wallet to access the CMS</p>
-            </div>
-        );
-    }
-
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] min-h-screen pb-20 gap-16 sm:p-20">
+        <div className="grid grid-rows-[20px_1fr_20px] min-h-screen pb-20 gap-16 sm:p-2">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
                 <h1 className="text-2xl font-bold">Proof of Site</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2  gap-6 w-full">
                     <div className="lg:row-span-2">
-                        <ReviewersCard />
+                        <div>
+
+
+                        </div>
                     </div>
                 </div>
             </main>

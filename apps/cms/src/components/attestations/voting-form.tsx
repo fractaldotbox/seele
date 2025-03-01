03fc2bcca9721f48e8b9739893ac1d4f4c59f076
metalboyrick <rsulisthio@gmail.com>
2025-03-01 13:58:02 -0700

    feat: fix toast

--- apps/cms/src/components/attestations/voting-form.tsx
+++ apps/cms/src/components/attestations/voting-form.tsx
@@ -1,18 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { ToastAction } from "@/components/ui/toast";
 import { toast } from "@/hooks/use-toast";
-import { getEasscanAttestationUrl } from "@/lib/eas/easscan";
-import { getShortHex } from "@/lib/utils/hex";
+import {
+  getEasscanAddressUrl,
+  getEasscanAttestationUrl,
+} from "@/lib/eas/easscan";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import type { Address } from "viem";
@@ -22,117 +24,120 @@ import { z } from "zod";
 // now focus on sdk part
 
 export interface VotingFormParams {
-	chainId: number;
-	isOffchain: boolean;
-	signAttestation: (data: string) => Promise<{
-		uids: Address[];
-		txnReceipt: { transactionHash: `0x${string}` };
-	}>;
-	isDisabled?: boolean;
+  chainId: number;
+  isOffchain: boolean;
+  signAttestation: (data: string) => Promise<{
+    uids: Address[];
+    txnReceipt: { transactionHash: `0x${string}` };
+  }>;
+  isDisabled?: boolean;
 }
 
 // TODO dynamic schema. For now, hardcode the MetIRL
 // https://github.com/fractaldotbox/geist-dapp-kit/issues/56
 export const VotingForm = ({
-	chainId,
-	isOffchain,
-	signAttestation,
-	isDisabled,
+  chainId,
+  isOffchain,
+  signAttestation,
+  isDisabled,
 }: VotingFormParams) => {
-	const formSchema = z.object({
-		voteFor: z.string(),
-	});
+  const formSchema = z.object({
+    voteFor: z.string(),
+  });
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			voteFor: "left",
-		},
-	});
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      voteFor: "left",
+    },
+  });
 
-	function onSubmit(values: z.infer<typeof formSchema>) {
-		signAttestation(values.voteFor).then(
-			({
-				uids,
-				txnReceipt,
-			}: {
-				uids: Address[];
-				txnReceipt: { transactionHash: `0x${string}` };
-			}) => {
-				const [uid] = uids;
-				const url = getEasscanAttestationUrl(chainId, uid, isOffchain);
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    signAttestation(values.voteFor).then(
+      ({
+        uids,
+        txnReceipt,
+      }: {
+        uids: Address[];
+        txnReceipt: { transactionHash: `0x${string}` };
+      }) => {
+        console.log("uids", uids);
 
-				const description = isOffchain
-					? getShortHex(uid)
-					: `attested ${txnReceipt?.transactionHash}`;
+        const [uid] = uids;
+        const url = getEasscanAttestationUrl(chainId, uid, isOffchain);
 
-				toast({
-					title: "Voting success",
-					description,
-					action: (
-						<ToastAction altText="View on EASSCAN">
-							<a target="_blank" href={url} rel="noreferrer">
-								View on EASSCAN
-							</a>
-						</ToastAction>
-					),
-				});
-			},
-		);
-	}
+        toast({
+          title: "Voting success",
+          description: `attested ${txnReceipt?.transactionHash}`,
+          action: (
+            <ToastAction altText="View on EASSCAN">
+              <a
+                target={`_blank`}
+                href={url}
+                rel="noreferrer"
+                className="text-blue-500 cursor-pointer hover:underline"
+              >
+                View on EASSCAN
+              </a>
+            </ToastAction>
+          ),
+        });
+      },
+    );
+  }
 
-	return (
-		<Card className="pt-8 w-full">
-			<CardContent>
-				<Form {...form}>
-					{/* HARDCODED */}
-					<form className="space-y-8">
-						<FormField
-							control={form.control}
-							name="voteFor"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Cast Your Vote</FormLabel>
-									<FormControl>
-										<div className="flex gap-4 w-full">
-											<Button
-												type="button"
-												onClick={() => {
-													field.onChange(
-														// TODO: change address to agent address
-														"0x30B00979c33F826BCF7e182545A3353aD97e1C42",
-													);
-													form.handleSubmit(onSubmit)();
-												}}
-												className="flex-1"
-												disabled={isDisabled}
-											>
-												Version A ⬅️
-											</Button>
-											<Button
-												type="button"
-												onClick={() => {
-													field.onChange(
-														// TODO: change address to agent address
-														"0xAaB311758eD38909734a14a4c19ae4BE3b700E61",
-													);
-													form.handleSubmit(onSubmit)();
-												}}
-												className="flex-1"
-												disabled={isDisabled}
-											>
-												Version B ➡️
-											</Button>
-										</div>
-									</FormControl>
-									<FormDescription>Vote for your ideal version</FormDescription>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-					</form>
-				</Form>
-			</CardContent>
-		</Card>
-	);
+  return (
+    <Card className="pt-8 w-full">
+      <CardContent>
+        <Form {...form}>
+          {/* HARDCODED */}
+          <form className="space-y-8">
+            <FormField
+              control={form.control}
+              name="voteFor"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Cast Your Vote</FormLabel>
+                  <FormControl>
+                    <div className="flex gap-4 w-full">
+                      <Button
+                        type="button"
+                        onClick={() => {
+                          field.onChange(
+                            // TODO: change address to agent address
+                            "0x30B00979c33F826BCF7e182545A3353aD97e1C42",
+                          );
+                          form.handleSubmit(onSubmit)();
+                        }}
+                        className="flex-1"
+                        disabled={isDisabled}
+                      >
+                        Version A ⬅️
+                      </Button>
+                      <Button
+                        type="button"
+                        onClick={() => {
+                          field.onChange(
+                            // TODO: change address to agent address
+                            "0xAaB311758eD38909734a14a4c19ae4BE3b700E61",
+                          );
+                          form.handleSubmit(onSubmit)();
+                        }}
+                        className="flex-1"
+                        disabled={isDisabled}
+                      >
+                        Version B ➡️
+                      </Button>
+                    </div>
+                  </FormControl>
+                  <FormDescription>Vote for your ideal version</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
 };
